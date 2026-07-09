45dbbcaa6e20a0b5e8f601e3a39434062eebe76e
Roman Lobun <rrw684@gmail.com>
2026-07-09 13:32:22 +0200

    AMateria.hpp

---
+++ ex03/inc/AMateria.cpp
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AMateria.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlobun <rlobun@student.42madrid.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/09 13:16:48 by rlobun            #+#    #+#             */
+/*   Updated: 2026/07/09 13:30:05 by rlobun           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef AMATERIA_H
+#define AMATERIA_H
+
+
+#include <iostream>
+#include "ICharacter.hpp"
+
+class AMateria
+{
+	protected:
+		std::string type;
+
+	public:
+
+	// Constructors
+
+		AMateria();
+		AMateria(std::string const type_name);
+		AMateria(const AMateria& other);
+	
+	// Deconstructor
+
+		virtual ~AMateria();
+	
+	// Operators 
+	
+		AMateria& opertator=(const AMateria &other);
+	
+	// Getters
+
+		const	std::string& getType() const;
+	
+	// Setters
+
+		void	setType(const std::string& type);
+	
+	// Utils
+		void	toString();
+
+		virtual AMateria* clone() const = 0;
+		virtual void use(ICharacter& target);
+};
+
+#endif
